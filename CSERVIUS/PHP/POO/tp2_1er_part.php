aaf31f2d8a0569c3224d08101e707623ec88ecfa
whoamitty <74999206+whoamitty@users.noreply.github.com>
2024-03-12 13:00:01 -0300

    CEJM chap 19,20  , dev POO php, annal Cyber New exos CEJM chap 19,20 finished dev fight game POO php new annal CYBER KOUDOU

--- CSERVIUS/PHP/POO/tp2_1er_part.php
+++ CSERVIUS/PHP/POO/tp2_1er_part.php
@@ -1,15 +1,35 @@
 <!-- 04-03-2024 14:42:44 -->
 <?php
 class Pont{
-    // unit="m²"
+    // unit="m²";
     // private string $unite = "m²";
+
     private const UNITE="m²";
     private float $longueur;
     private float $largeur;
     // 5*2='10 m2';
 
+    public function __construct(float $longueur,float $largeur)
+    {
+        self::validerTaille($longueur);
+        $this -> longueur = $longueur;
+
+        self::validerTaille($largeur);
+        $this -> largeur = $largeur;
+        
+    }
+
+    /* public function __destruct()
+    {
+        print 'Destroying' . __CLASS__ . "\n" ;
+    } */
+
+    public function __toString(): string{
+        return 'Longeur:' . $this->longueur;
+    }
+
     public static function validerTaille(float $taille): bool {
-        if ($taille < 0){
+        if ($taille <= 0){
             trigger_error('La longueur ne doit pas être négative', E_USER_ERROR);
             return false;
         }
@@ -33,15 +53,20 @@ class Pont{
         return $this->longueur*$this->largeur . self::UNITE;
     }
 }
+/* $bridge-> setLargeur(1200.5);
+$bridge-> setLongueur(22); */
 
-$bridge = new Pont();
+$PontC = new Pont(110,2299);
+
 // $bridge->longueur=1200;
 // $bridge->largeur=15;
 
 /* $bridge-> setLargeur(1200.5);
 $bridge-> setLongueur(15); */
 
-$bridge-> setLargeur(1200.5);
-$bridge-> setLongueur(22);
-echo $bridge->getSurface();
+// echo $PontC->getSurface();
+
+echo $PontC;
+
+// echo $bridge->getSurface();
 
